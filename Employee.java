764991c0a126736808ac2e6d64a608bc0974d5c4
Irenevn16 <irenevn16@gmail.com>
2025-04-02 17:38:36 +0200

    setters and getters

--- Employee.java
+++ Employee.java
@@ -1,3 +1,39 @@
 public class Employee {
 
+    private String name;
+    private String email;
+    private int age;
+    private int salary;
+
+    public Employee (String name, String email, int age, int salary) {
+        setName(name);
+        setEmail(email);
+        setAge(age);
+        setSalary(salary);
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setSalary(int salary{
+        this.salary = salary;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public int getAge(){
+        return age;
+    }
+    public int getSalary(){
+        return salary;
+    }
+
 }
