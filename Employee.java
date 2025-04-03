8cd5542b5bed49b98b86759177617c9a1bc36be6
Irenevn16 <irenevn16@gmail.com>
2025-04-03 14:57:23 +0200

    intern

--- Employee.java
+++ Employee.java
@@ -1,11 +1,16 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Employee {
 
     private String name;
     private String email;
     private int age;
-    private int salary;
+    private double salary;
 
-    public Employee (String name, String email, int age, int salary) {
+    public Employee (String name, String email, int age, double salary) {
         setName(name);
         setEmail(email);
         setAge(age);
@@ -20,7 +25,7 @@ public class Employee {
     public void setAge(int age) {
         this.age = age;
     }
-    public void setSalary(int salary{
+    public void setSalary(double salary) {
         this.salary = salary;
     }
     public String getName() {
@@ -32,8 +37,11 @@ public class Employee {
     public int getAge(){
         return age;
     }
-    public int getSalary(){
+    public double getSalary(){
         return salary;
     }
 
+    public String toString(){//para que devuelva las propiedades como String
+        return "Name: " + name + ", Email: " + email + ", Age: " + age + ", Salary: " + salary;
+    }
 }
--- Intern.java
+++ Intern.java
@@ -1,2 +1,16 @@
-public class Intern {
+public class Intern extends Employee {
+    private static final double MAX_SALARY = 20000; //salario máx para intern
+
+    public Intern (String name, String email, int age, double salary) {
+        super(name, email, age, Math.min(salary, MAX_SALARY));//para que el salario no se pase del max
+    }
+
+    @Override //a getSalary para que el salario no supere el max
+    public void setSalary(double salary) {
+        if (salary > MAX_SALARY) {
+            System.out.println("Error: el salario no puede superar " + MAX_SALARY);
+        } else {
+            super.setSalary(salary);
+        }
+    }
 }
